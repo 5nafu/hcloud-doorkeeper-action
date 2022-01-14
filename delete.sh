4ed01a0c74cc37ea79bd66b072e8eeec4b32f04d
5nafu <info@tvollmer.de>
2022-01-14 23:17:32 +0100

    Use Options

--- delete.sh
+++ delete.sh
@@ -1,7 +1,14 @@
 #! /bin/sh
 
+while getopts n:p:t:i: option
+do
+    case "$option" in
+        n) echo "Name=$OPTARG";;
+        p) echo "Port=$OPTARG";;
+        t) echo "Protocol=$OPTARG";;
+        i) echo "IP=$OPTARG";;
+    esac
+done
+echo "----------------------------"
 echo "Env:"
 env
-echo "----------------------------"
-echo "Arguments:"
-echo "$@"
