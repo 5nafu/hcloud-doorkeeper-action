ff53508c56a2440212a48608aec543ccc83fc558
5nafu <info@tvollmer.de>
2022-01-15 00:07:52 +0100

    Use hcloud

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 # Container image that runs your code
+FROM boky/hcloud AS hcloud
 FROM alpine:3.10
 
-# Copies your code file from your action repository to the filesystem path `/` of the container
+COPY --from=hcloud /hcloud /usr/local/bin/hcloud
+
 COPY create.sh /create.sh
-COPY delete.sh /delete.sh
+RUN ln -s /create.sh /delete.sh
 
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
 ENTRYPOINT ["/create.sh"]
--- create.sh
+++ create.sh
@@ -1,14 +1,32 @@
 #! /bin/sh
 
-while getopts n:p:t:i: option
-do
-    case "$option" in
-        n) echo "Name=$OPTARG";;
-        p) echo "Port=$OPTARG";;
-        t) echo "Protocol=$OPTARG";;
-        i) echo "IP=$OPTARG";;
-    esac
-done
-echo "----------------------------"
 echo "Env:"
 env
+echo "----------------------------"
+if [[ -n "$INPUT_IP" ]]; then
+    IP_RANGE="$INPUT_IP"
+else
+    echo "Getting Public IP..."
+    IP_RANGE="$(wget -O- -q http://ifconfig.me/ip )/32"
+fi
+
+case $0 in
+    create.sh)
+        ACTION=add-rule
+        ;;
+    delete.sh)
+        ACTION=delete-rule
+        if [[ "$INPUT_AUTOCLEAN" != "true" ]]; then
+            echo "Cleanup not required due to options"
+            exit 0
+        fi
+        ;;
+esac
+
+echo /usr/local/bin/hcloud firewall $ACTION \
+    $INPUT_FIREWALL_NAME \
+    --direction in \
+    --port ${{ INPUT_PORT }} \
+    --protocol $INPUT_PROTOCOL \
+    --source-ips $IP_RANGE \
+    --description Doorkeeper Rule - Repository $GITHUB_REPOSITORY - Workflow: $GITHUB_WORKFLOW/$GITHUB_RUN_NUMBER/$GITHUB_RUN_ATTEMPT
