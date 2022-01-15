04d35f5ca62af6b23f89732983a39f464282a852
5nafu <info@tvollmer.de>
2022-01-15 21:18:58 +0100

    Fixing architecture

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
 # Container image that runs your code
 FROM debian:11-slim
 
-ARG TARGETOS 
-ARG TARGETARCH
-
 COPY scripts/ /usr/local/bin/
 RUN /usr/local/bin/install.sh
 
--- scripts/install.sh
+++ scripts/install.sh
@@ -13,9 +13,10 @@ ln -s /usr/local/bin/create.sh /usr/local/bin/delete.sh
 # Get latest hcloud version from GitHub
 API="https://api.github.com/repos/hetznercloud/cli/releases/latest"
 HCLOUD_VERSION=$(wget -q -O- $API  | grep '"tag_name":'| cut -d'"' -f4)
+ARCH=$(uname -m)
+case $ARCH in
+    "aarch64") ARCH=arm64
+esac
 
-echo "TARGETOS: '$TARGETOS' TARGETARCH: '$TARGETARCH'"
-uname -a
-
-echo "Downloading version '$HCLOUD_VERSION' from GitHub..."
-wget -O- https://github.com/hetznercloud/cli/releases/download/$HCLOUD_VERSION/hcloud-${TARGETOS:-linux}-${TARGETARCH:-amd64}.tar.gz | tar xzC /usr/local/bin hcloud
+echo "Downloading '$ARCH' hcloud, version '$HCLOUD_VERSION', from GitHub..."
+wget -O- https://github.com/hetznercloud/cli/releases/download/$HCLOUD_VERSION/hcloud-linux-$ARCH.tar.gz | tar xzC /usr/local/bin hcloud
