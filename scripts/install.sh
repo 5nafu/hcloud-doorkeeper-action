c4d410155779c851e611e9c0f65d4bc2c5fb1389
5nafu <info@tvollmer.de>
2022-01-15 21:01:21 +0100

    Debug

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,11 +14,8 @@ ln -s /usr/local/bin/create.sh /usr/local/bin/delete.sh
 API="https://api.github.com/repos/hetznercloud/cli/releases/latest"
 HCLOUD_VERSION=$(wget -q -O- $API  | grep '"tag_name":'| cut -d'"' -f4)
 
-if [[ -n "$TARGETOS" ]] || [[ -n "$TARGETARCH" ]]; then
-    echo "target OS or ARCH unknown. uname shows:"
-    uname -a
-    echo "Using defaults: Linux & amd64"
-fi
+echo "TARGETOS: '$TARGETOS' TARGETARCH: '$TARGETARCH'"
+uname -a
 
 echo "Downloading version '$HCLOUD_VERSION' from GitHub..."
 wget -O- https://github.com/hetznercloud/cli/releases/download/$HCLOUD_VERSION/hcloud-${TARGETOS:-linux}-${TARGETARCH:-amd64}.tar.gz | tar xzC /usr/local/bin hcloud
